78a119466cbd2a1d27306b6f63543d19e2c1620f
juan.salmeron <jnssterrass@gmail.com>
2017-10-14 14:52:38 +0200

    server hello

--- CockfightSentenceGenerator/main.js
+++ CockfightSentenceGenerator/main.js
@@ -3,28 +3,29 @@ var app         = express();
 var bodyParser  = require('body-parser');
 var Sentencer   = require('sentencer');
 
+nounList = [
+    "Laura", "Juan", "Edu"
+];
 
-
-/*
 // Sentencer
 Sentencer.configure({
     // the list of nouns to use. Sentencer provides its own if you don't have one!
-    nounList: [],
+    nounList: nounList,
 
     // the list of adjectives to use. Again, Sentencer comes with one!
-    adjectiveList: [],
+    //adjectiveList: [],
 
     // additional actions for the template engine to use.
     // you can also redefine the preset actions here if you need to.
     // See the "Add your own actions" section below.
-    actions: {
+    /*actions: {
         my_action: function(){
             return "something";
         }
-    }
+    }*/
 });
-*/
-console.log(Sentencer.make("This sentence has {{ a_noun }} and {{ an_adjective }} {{ noun }} in it."));
+
+console.log(Sentencer.make("This sentence is from {{ noun }} and {{ an_adjective }} {{ noun }} in it."));
 
 
 app.get('/', function (req, res) {
@@ -36,5 +37,5 @@ app.get('/rap', function (req, res) {
 });
 
 app.listen(80, function () {
-    console.log('Example app listening on port 80!');
+    console.log('TwitterCockFight listening on port 80!');
 });
